d7a5a14977063cc41616aefeb1ce6ac93e664952
telmo <telmosalvado18@gmail.com>
2021-02-27 15:23:45 +0000

    Layout Transporte

---
+++ app/src/main/java/pt/ipg/viagens/NovoTransporte.java
@@ -0,0 +1,14 @@
+package pt.ipg.viagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovoTransporte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_transporte);
+    }
+}
---
+++ app/src/main/java/pt/ipg/viagens/TransporteActivity.java
@@ -0,0 +1,14 @@
+package pt.ipg.viagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransporteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transporte);
+    }
+}
