c9cf70653b9655b534f5575a70f86de995a0b7f8
telmo <telmosalvado18@gmail.com>
2021-02-27 11:52:18 +0000

    Criação da interface Nova viagem

--- app/src/main/java/pt/ipg/viagens/NovaViagem.java
+++ app/src/main/java/pt/ipg/viagens/NovaViagem.java
@@ -2,7 +2,9 @@ package pt.ipg.viagens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NovaViagem extends AppCompatActivity {
 
@@ -11,4 +13,5 @@ public class NovaViagem extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nova_viagem);
     }
+
 }
--- app/src/main/java/pt/ipg/viagens/ViagensActivity.java
+++ app/src/main/java/pt/ipg/viagens/ViagensActivity.java
@@ -2,7 +2,9 @@ package pt.ipg.viagens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViagensActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class ViagensActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viagens);
     }
+    public void  NovaViagem(View view){
+        Intent intent = new Intent(this, NovaViagem.class);
+        startActivity(intent);
+    }
+    public void  VerViagem(View view){
+        Intent intent = new Intent(this, VerViagem.class);
+        startActivity(intent);
+    }
+    public void  ApagarViagem(View view){
+        Intent intent = new Intent(this, EliminarViagem.class);
+        startActivity(intent);
+    }
+
 }
